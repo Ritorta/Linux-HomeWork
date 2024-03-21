b0f9474b3fb2a60f43ea856895e7395a44db939f
Ritorta <Meddesu@yandex.ru>
2024-03-21 13:20:13 +0500

    upgrade ownersort.sh

--- MainLinux/Work#8/ownersort.sh
+++ MainLinux/Work#8/ownersort.sh
@@ -7,6 +7,20 @@ source_dir="/path/to/source"
 echo "The path to the folder where the files will be copied: "
 destination_dir="/path/to/destination"
 
+log_file="ownersort.log"
+
+# Check if source directory exists
+if [ ! -d "$source_dir" ]; then
+    echo "Error: Source directory does not exist."
+    exit 1
+fi
+
+# Check if destination directory exists
+if [ ! -d "$destination_dir" ]; then
+    echo "Error: Destination directory does not exist."
+    exit 1
+fi
+
 cd "$source_dir" || exit
 
 files=$(find . -type f)
@@ -17,16 +31,15 @@ for file in $files; do
 
     if [ -d "$destination_dir/$owner" ]; then
 
-        cp "$file" "$destination_dir/$owner/$file"
-        echo "File '$file' copy in folder '$destination_dir/$owner'"
+        cp "$file" "$destination_dir/$owner/$file" || echo "Error: Failed in copy $file to $destination_dir/$owner"
+        echo "$(date) - File '$file' copied in folder '$destination_dir/$owner'" >> "$log_file"
 
     else
 
         mkdir -p "$destination_dir/$owner"
 
-        cp "$file" "$destination_dir/$owner/$file"
-
-        echo "File '$file' copy in new folder '$destination_dir/$owner'"
+        cp "$file" "$destination_dir/$owner/$file" || echo "Error: Failed in copy $file to $destination_dir/$owner"
+        echo "$(date) - File '$file' copied in new folder '$destination_dir/$owner'" >> "$log_file"
 
     fi
 
